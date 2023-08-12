78dffa4fddea0151789c0594f13e829b90089f35
phuocnehihi1 <130758054+phuocnehihi1@users.noreply.github.com>
2023-08-12 14:17:20 +0700

    done sign up

--- CrreateWebSiteMusic/js/login.js
+++ CrreateWebSiteMusic/js/login.js
@@ -1,17 +1,27 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+
 const urlApi = ' http://localhost:3000/user';
 
  const user_mail = $('.user_mail');
  const user_password = $('.user_password');
  const login =$('.btn-submit');
+ const error_active = $('header .left_bx1 .content .l-card .error');
+
 // FuntionCheck Email
  function validateEmail(email) {
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
     return emailPattern.test(email);
 }
 function getAPI(callback){
-    fetch(urlApi)
+    const object ={
+
+        method: "GET", // Đặt phương thức là GET
+        headers: {
+            "Content-Type": "application/json" // Đặt header cho yêu cầu (tuỳ chọn)
+        }
+    }
+    fetch(urlApi,object)
         .then(function(response){
             return response.json();
         })
@@ -23,20 +33,26 @@ function handleEvent(){
     login.onclick = function(){
         const mail = user_mail.value;
         const password = user_password.value;
-       // if(validateEmail(mail)){
+       if(validateEmail(mail)){
+            error_active.classList.remove('active');
+            
             getAPI(function(result){
-                const i = 0;
-                for(; result.length; i++){
-                    if(mail == result[i].email){
-                       console.log(result)
+
+                for(const user of result){
+                    if(user.email === mail && user.password === password){
+                        console.log(user.id);
+                        error_active.classList.remove('active');
+                    }else{
+                        error_active.classList.add('active');
+                        error_active.innerHTML = "The email of the password is incorrect";
+
                     }
                 }
             })
 
-
-      //  }else{
-        //    alert("d......");
-        //}
+       }else{
+           error_active.classList.add('active');
+        }
         
 
     }
--- CrreateWebSiteMusic/js/signup.js
+++ CrreateWebSiteMusic/js/signup.js
@@ -14,6 +14,7 @@ const btn_submit = $('.btn-submit');
 
 function start(){
     handleEvent();
+    
 }
 
 
@@ -54,48 +55,99 @@ function validateEmail(email) {
     return emailPattern.test(email);
 }
 
+// Check value Number
+function validateNumber(number){
+    var numberPattern = /^[0-9]+$/;
+    return  numberPattern.test(number);
+}
+// Dem Vlue
+function valueNumbe(number){
+    for(let i = 0; i < number.length; i++){
+        if(isNaN(parseInt(number[i]))){
+            return false;
+        }
+    }
+    return true;
+}
+function validatePassword(password){
+    var passwordPattern = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    return passwordPattern.test(password)
+}
 function handleEvent() {
+    const status = false;
     btn_submit.onclick = function () {
         const name = user_name.value;
         const email = user_emai.value;
         const password = user_password.value;
         const re_password = re_user_password.value;
-        const contact = use_phone.value;    
+        let contact = use_phone.value;    
         if (name !== "" && email !== "" && password !== "" && re_password !== "" && contact !== "") {
             
             if (password === re_password) {
-                if(validateEmail(email)){
-                    const formData = {
-                        name: name,
-                        email: email,
-                        password: password,
-                        contact: contact
-                    };
-                    console.log(formData);
-                    CreateUser(formData);
-                     alert("You have successfully created an account"); 
+                if(validatePassword(password) && password.length >=8){
+                // if check Formaty email
+                    if(validateEmail(email)){
+                        getResultuser(function(results){
+                            const CheckisEmail = results.some(function(isEmail){
+                            return  isEmail.email === email;
+                            })
+                            if(!CheckisEmail){
+                                if(valueNumbe(contact) && contact.length === 10){
+                                const data = {
+                                    name: name,
+                                    mail: email,
+                                    password: password,
+                                    contact: contact
+                                }
+                                CreateUser(data);
+                                alert("Account successfully created")
+                            
+                                }else{
+
+                                    alert("Your Phone format is incorrect, please re-enter it");
+                                    use_phone.focus();
+                                }
+
+                            }else{
+                                alert("Email already exists");
+                            }
+                        })
+                    }
+                // else  format email
+                    else{
+                        alert("Your email format is incorrect, please re-enter it");
+                        user_emai.focus();
+                    }
                 }else{
-                    alert("Inappropriate email format "); 
-                }     
+                    alert("Format Your password is incorrect, please min 1 capital letter 1 special character and 1 number, minimum length 8 characters");
+                    user_password.focus();
+                }  
+                } else {
+                    alert("Password not Match");
+                    user_password.focus();
+                }
+            
             } else {
-                alert("Password not Match");
-                user_password.focus();
-            }
-        } else {
-            alert("Enter full information");
-            if(name == ""){
-                user_name.focus();
-            }else if(email == ""){
+               
+                if(name == ""){
+                    alert("Enter Name information");
+                    user_name.focus();
+                }else if(email == ""){
+                    alert("Enter Email information");
                 user_emai.focus();
-            }else if(password == ""){
-                user_password.focus();
-            }else if(re_password ==""){
-                re_user_password.focus();
-            } else if(contact == ""){
-                use_phone.focus();
+                }else if(password == ""){
+                    alert("Enter Password information");
+                    user_password.focus();
+                    
+                }else if(re_password ==""){
+                    re_user_password.focus();
+                    alert("Enter Re Password information");
+                } else if(contact == ""){
+                    alert("Enter Phone Number information");
+                    use_phone.focus();
+                }
             }
-        }
-    };
-}
+        };
+    }
 // Start Call Api User 
 start();
